931f2c26dd2343bbc340b22a2fa9743374d311d2
Dawa Tshering <dawatshering@Dawas-MacBook-Pro.local>
2018-01-12 15:59:22 +0600

    Dawa's update- Events Page, Speaker, and Mentor

--- src/pages/venue/venue.module.ts
+++ src/pages/venue/venue.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { FeedbackPage } from './venue';
+import { VenuePage } from './venue';
 
 @NgModule({
   declarations: [
